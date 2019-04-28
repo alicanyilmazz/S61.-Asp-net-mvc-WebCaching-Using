69502a8b608a0207bc91ff9333c8da7aee7aedbd
alicanyilmazz <alicanyilmazz@users.noreply.github.com>
2019-04-29 01:25:54 +0300

    Add project files.

---
+++ WebCache_Using/Controllers/MyWebCacheController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace WebCache_Using.Controllers
+{
+    public class MyWebCacheController : Controller
+    {
+        // GET: MyWebCache
+        public ActionResult Index()
+        {
+            string time = WebCache.Get("zaman");
+
+            if (string.IsNullOrEmpty(time))
+            {
+                time = DateTime.Now.ToString();
+                WebCache.Set(key: "zaman", value: time, minutesToCache: 1, slidingExpiration: true);
+            }
+
+            ViewBag.simdi = time;
+
+            return View();
+        }
+
+        public ActionResult RemoveCache()
+        {
+            WebCache.Remove("zaman");
+            return RedirectToAction("Index");
+        }
+
+    }
+}
+
+//Örnegin menüdeki kategoriler sürekli değişmiyorsa veya sürekli değişmeyen menüler vs Cache den cekilebilir.
